838bdd1e1be1d6521d5a6cc73620c5d25301b2fc
uzn1023 <e5305@gm.kochi-ct.jp>
2021-11-01 15:58:13 +0900

    Read from Excel and suggest next paras (sample program from web site)

---
+++ excel2bayes.py
@@ -0,0 +1,66 @@
+##!/usr/bin/env python
+
+import xlrd
+import GPyOpt
+import numpy as np
+
+#excelファイルに読み込み
+wb = xlrd.open_workbook("./BO_test.xlsx")
+print(wb.sheet_names())
+sheet = wb.sheet_by_name('Sheet1')
+columns = [sheet.col_values(i) for i in range(sheet.ncols)]
+
+inputs = []
+inputs_numtype = []
+inputs_domain = []
+inputs_name = []
+outputs = []
+outputs_name = []
+for i, column in enumerate(columns):
+    if column[1] == "i":
+        inputs_name.append(column[0])
+        inputs.append(column[5:])
+        inputs_numtype.append(column[2])
+        inputs_domain.append([float(column[3]),float(column[4])])
+    if column[1] == "o":
+        outputs_name.append(column[0])
+        outputs.append(column[5:])
+
+#予備実験のデータ
+initial_x = np.array(inputs).T
+initial_y = np.array(outputs[0])
+
+initial_y = initial_y.reshape((-1,1))
+print(initial_x)
+
+def f(x):
+    print("next x")
+    for n,value in zip(inputs_name,x[0]):
+        print(n + ":" + str(value))
+    score = float(input("Input y : "))
+    return score
+
+bounds = []
+for i,input_name in enumerate(inputs_name):
+    bound = {}
+    bound["name"] = input_name
+    if inputs_numtype[i] == "c":
+        bound["type"] = "continuous"
+    elif inputs_numtype[i] == "d":
+        bound["type"] = "discrete"
+    bound["domain"] = tuple(inputs_domain[i])
+    
+    bounds.append(bound)
+
+myBopt = GPyOpt.methods.BayesianOptimization(f=f,
+                                             domain=bounds,
+                                             X = initial_x,
+                                             Y = initial_y,
+                                             normalize_Y=False,
+                                             model_type = "GP",
+                                             acquisition_type='LCB',               
+                                             )
+
+
+for i in range(30):#このfor文は人間が回す
+    myBopt.run_optimization(max_iter=1)
